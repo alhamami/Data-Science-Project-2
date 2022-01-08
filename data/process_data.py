83a0647566c0cb0ac09e56c171e072a248250e2e
alhamami <89762635+alhamami@users.noreply.github.com>
2022-01-08 05:59:58 +0300

    Add files via upload

--- data/process_data.py
+++ data/process_data.py
@@ -3,38 +3,81 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def split(categ):
-    cates = (categ['categories'].str.split(pat=';', expand=True)).iloc[0, :]
-    return cates.map(lambda x: x.split('-')[0])
+
+def concat2F(file2, file1):
+    """
+    The function will concat DataFrames
     
-def concat2F(messages_filepath, file1):
-    file2 = pd.read_csv(messages_filepath)
+    Parameters:
+    file1 (DataFrame): the categories DataFrame that will be splitting
+    
+    Returns:
+    df (dataframe): the categories DataFrame after split it
+    """
     return pd.concat([file2, file1], axis=1)
 
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    The function will loads data from two files and then it will merges the two files in one dataframe after convert categories to 0, 1
     
+    Parameters:
+    messages_filepath (str): filepath to the csv containing disaster relief messages
+    categories_filepath (str):  filepath to the csv containing the multi-classifications of the above messages
+    
+    Returns:
+    df (dataframe): The dataset that contains the messages and the classifications
+    """
+
     file1 = pd.read_csv(categories_filepath)
-    file1.columns = split(file1)
+
+    cates = (file1['categories'].str.split(pat=';', expand=True)).iloc[0, :]
+    file1.columns =  cates.map(lambda x: x.split('-')[0])
+
 
     for x in file1:
         file1[x] = file1[x].map(lambda x: x[-1])
         file1[x] = file1[x].astype(int)
 
-    data = concat2F(messages_filepath, file1.replace(2, 1, inplace=True))
+    file1.replace(2, 1, inplace=True)
+    file2 = pd.read_csv(messages_filepath)
+    data = concat2F(file2, file1)
     return data
     
 
-def clean_data(df):     
+def clean_data(df): 
+    """
+    The function will remove the duplicates in the dataframe
+    
+    Parameters:
+    df (dataframe): The dataset that contains the messages and the classifications
+    
+    Returns:
+    df (dataframe): The cleaned dataset
+    """    
     return df.drop_duplicates()
           
           
 def save_data(df, database_filename):
+    """
+    The function will saves the data in SQLite database
+    
+    Parameters:
+    df (dataframe): A cleaned dataset
+    database_filename (str): The name of the database file to be created - should end in .db.  
+    
+    Returns:
+    The database file with the above specified name
+    """
     df.to_sql('disaster_response_df',
      create_engine('sqlite:///{}'.format(database_filename)), index=False)
 
 
 def main():
+    """
+    The function will grabs the required variables from the command line,
+    stacks the above functions in the right order and generates a clean .db file.
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
